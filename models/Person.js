f1b45b9b8f4c7559ed9c741618758d40cceeb9fd
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-01-21 14:18:24 +0530

    initializing bcrypt

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 // Defining person's schema
 const personSchema = new mongoose.Schema({
@@ -41,6 +42,38 @@ const personSchema = new mongoose.Schema({
     }
 })
 
+personSchema.pre('save', async function(next) {
+    const person = this;
+
+    // Hash the password only if the password is modified or new person's data is saved
+    if(!person.isModified('password')) return next();
+
+    try {
+        // Hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // Hashed password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    } catch (err) {
+        return next(err);
+    }
+})
+
+// This method is used in the auth.js file for comparing the password
+personSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        // Using bcrypt to compare the provided password with the hashed password(stored)
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    } catch (err) {
+        throw err;
+    }
+}
+
 // Creating person's model
-const Person = mongoose.model('person', personSchema);
+const Person = mongoose.model('Person', personSchema);
 module.exports = Person;

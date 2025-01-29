15d8b5ee65c0a52bde8713de9722cc3d24932c8e
Mukesh704 <150328584+Mukesh704@users.noreply.github.com>
2025-01-29 13:03:35 +0530

    using token for authorization

--- jwt.js
+++ jwt.js
@@ -1,7 +1,12 @@
 const jwt = require('jsonwebtoken')
 
 const jwtAuthMiddleware = (req, res, next) => {
-    const token = req.header.authorization.split(" ")[1];
+    // First check if request headers has authorization or not
+    const authorization = req.headers.authorization
+    if(!authorization) return res.status(401).json({error: 'Token Not Found'});
+
+        // Extract the jwt token from the request headers
+    const token = req.headers.authorization.split(" ")[1];
     if(!token) {
         return res.status(401).json({
             error: 'Unauthorized'
@@ -25,7 +30,7 @@ const jwtAuthMiddleware = (req, res, next) => {
 
 // Function to generate the jwt token
 const generateToken = (userData) => {
-    return jwt.sign(userData, process.env.JWT_SECRET_KEY, {expiresIn: 30});
+    return jwt.sign(userData, process.env.JWT_SECRET_KEY, {expiresIn: 30000});
 }
 
 module.exports = {jwtAuthMiddleware, generateToken}
